1cb760e7f73b47ecc7885a8f1a6877e4470e5178
phongctnguyen <phongctnguyen@gmail.com>
2020-10-25 20:17:31 +0700

    Finish setup graphql server

--- graphql-prisma-1/src/utils/generateToken.js
+++ graphql-prisma-1/src/utils/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (userId) =>
-  jwt.sign({ userId }, 'secret', { expiresIn: '7 days' });
+  jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7 days' });
 
 export default generateToken;
